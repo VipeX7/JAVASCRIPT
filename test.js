afc3ea868186784b28f8f223c5b02d9e77e77b89
VipeX7 <97938859+VipeX7@users.noreply.github.com>
2025-06-12 17:38:05 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,12 @@
+
+const accountID = 14455;
+let accountEmail =  "sfretgre@gmag";
+var accountpass = "1234567890";
+accountcity= "New York";
+
+accountEmail ="pp@hotmail"
+accountpass = "0987%#$@1";
+accountcity = "Delhi";
+console.log(accountID);
+
+console.table({accountEmail, accountpass, accountcity}); //For creating table
